fdb9e4ccf06383fbfb8e810020f21c874d64e62c
Austin BOOB <aaustinc@live.com>
2018-09-01 16:46:53 -0400

    intial add

---
+++ Input.js
@@ -0,0 +1,75 @@
+document.addEventListener("keydown", this.keyDown);
+document.addEventListener("keyup", this.keyUp);
+
+let Input ={}
+
+Input.left = 0;
+Input.right = 0;
+Input.down = 0;
+Input.spacebar = 0;
+var delta = 400;
+var lastKeypressTime = 0;
+
+var dblTapBool;
+
+function keyDown(event){
+    if (event.key == "ArrowRight" || event.key == "d") {
+        Input.right = 1;
+    }
+    if (event.key == "ArrowLeft" || event.key == "a") {
+        Input.left = 1;
+        }
+    if (event.key == "ArrowDown" || event.key == "s") {
+        Input.down = 1;
+        }
+    if(event.key == " "){
+        Input.spacebar = 1;
+        
+    }
+        var thisKeypressTime = new Date();
+        if ( thisKeypressTime - lastKeypressTime <= delta )
+        {
+        dblTap();
+        thisKeypressTime = 0;
+        }
+    lastKeypressTime = thisKeypressTime;
+    document.removeEventListener("keydown",keyDown);
+}
+
+function keyUp(event){
+    if (event.key == "ArrowRight" || event.key == "d") {
+        Input.right = 0;
+    }
+    if (event.key == "ArrowLeft" || event.key == "a") {
+        Input.left = 0;
+    }
+    if (event.key == "ArrowDown" || event.key == "s") {
+        Input.down = 0;
+    }
+    if(event.key == " "){
+        Input.spacebar = 0;
+  
+    }
+    dblTapBool=false;
+    document.addEventListener("keydown",keyDown);
+}
+
+
+function dblTap(){
+//console.log("DOUBLEDOO");
+dblTapBool = true;
+}
+
+
+ctx.beginPath();
+   
+    ctx.fillStyle="white";
+    
+    ctx.rect(580,500,50,30);
+    ctx.fill();
+    ctx.stroke();
+    ctx.strokeText("POWER!",582,515);
+    //ctx.strokeText();
+ctx.closePath();
+
+
